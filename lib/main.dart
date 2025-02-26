04c9034b7b553813df5fcf001b977ef89f069394
ANDRES_MIGUEL_RIVERA_FRANCISCO <a.22308051281295@cbtis128.edu.mx>
2025-02-26 20:09:59 +0000

    Andres Rivera

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyIconsAPP());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyIconsAPP extends StatelessWidget {
+  const MyIconsAPP({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Andres Rivera | 22308051281295'),
+            centerTitle: true,
+            backgroundColor: Color(0xff460840),
+            titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text(
+                'Andres Rivera | 22308051281295',
+                style: TextStyle(color: Color(0xffa80000), fontSize: 20),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(
+                    Icons.add_a_photo,
+                    color: Colors.pink,
+                    size: 24.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  Icon(
+                    Icons.backpack,
+                    color: Colors.green,
+                    size: 30.0,
+                  ),
+                  Icon(
+                    Icons.shopping_cart,
+                    color: Colors.blue,
+                    size: 36.0,
+                  ),
+                  Icon(
+                    Icons.apple,
+                    color: Color(0xffc6e82f),
+                    size: 36.0,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
+  } //end widgets
+} //end class MyIconsApp
